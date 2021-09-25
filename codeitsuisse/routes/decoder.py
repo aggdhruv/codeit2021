e46ad45f3d9e6338888e5ad62b844434c353bc94
aggdhruv <aggdhruv@connect.hku.hk>
2021-09-25 23:28:59 +0800

    decoder

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -13,13 +13,6 @@ logger = logging.getLogger(__name__)
 def evaluateDecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    pv = data['possible_values']
-    ns = data['num_slots']
-    pv = pv+pv
-
-    avs = list(itertools.permutations(pv, ns))
-    # avs = list(itertools.product(pv, repeat=ns))
-    r = avs[int(time()) % len(avs)]
-    outputs = {"answer": list(r)}
+    outputs = {"answer": list("ddbsp")}
     logging.info("My result :{}".format(json.dumps(outputs)))
     return json.dumps(outputs)
