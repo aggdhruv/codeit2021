b279f3a063147da02c6bb7c26063b0b15eab3107
aggdhruv <aggdhruv@connect.hku.hk>
2021-09-25 21:33:06 +0800

    Decoder

---
+++ codeitsuisse/routes/decoder.py
@@ -0,0 +1,23 @@
+import logging
+import json
+import itertools
+from time import time
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(name)
+
+@app.route('/decoder', methods=['POST'])
+def evaluateQ():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    pv = data['possible_values']
+    ns = data['num_slots']
+
+    avs = list(itertools.product(pv, repeat = ns))
+    r = avs[int(time()) % len(avs)]
+    outputs = {"answer": list(r)}
+    logging.info("My result :{}".format(json.dumps(outputs)))
+    return json.dumps(outputs)
