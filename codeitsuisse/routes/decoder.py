d7cdfcc983bfd09fbcf7b33acd829f8965ab004c
aggdhruv <aggdhruv@connect.hku.hk>
2021-09-26 03:21:13 +0800

    corrected tictactoe

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -11,8 +11,15 @@ logger = logging.getLogger(__name__)
 
 @app.route('/decoder', methods=['POST'])
 def evaluateDecoder():
-    data = request.get_json()
+    data = request.get_json() 
     logging.info("data sent for evaluation {}".format(data))
-    outputs = {"answer": list("oaxol")}
+    pv = data['possible_values']
+    ns = data['num_slots']
+    pv = pv+pv
+
+    avs = list(itertools.permutations(pv, ns))
+    # avs = list(itertools.product(pv, repeat=ns))
+    r = avs[int(time()) % len(avs)]
+    outputs = {"answer": list(r)}
     logging.info("My result :{}".format(json.dumps(outputs)))
     return json.dumps(outputs)
