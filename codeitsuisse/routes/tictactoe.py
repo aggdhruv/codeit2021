748067791b2e9c4239b012724a8ea885e152f1ac
aggdhruv <aggdhruv@connect.hku.hk>
2021-09-26 01:11:29 +0800

    tic-tac-toe

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -9,6 +9,18 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 posMap = {
+    (0,0):"NW",
+    (0,1):"N",
+    (0,2):'NE',
+    (1,0):'W',
+    (1,1):'C',
+    (1,2):'E',
+    (2,0):'SW',
+    (2,1):'S',
+    (2,2):'SE'
+}
+
+checkMap = {
     "NW": (0,0),
     "N": (0,1),
     "NE": (0,2),
@@ -20,116 +32,162 @@ posMap = {
     "SE":(2,2)
 }
 
+
 rMap = dict(zip(posMap.values(), posMap.keys()))
 
-def checkWins(grid, player):
-    #rows
-    for ri, i in enumerate(grid):
-        if i.count(player) == 2 and i.count('Z') == 1:
-            return ('R', ri, i.index('Z'))
-    
-    #cols
-    for ci in range(3):
-        col = [r[ci] for r in grid]
-        if col.count(player) == 2 and col.count('Z') == 1:
-            return ('C', col.index('Z'), ci)
+player,opponent = ['O','X']
+board = [['_','_','_'],
+        ['_','_','_'],
+        ['_','_','_']]
 
-    #diags
-    d1 = [grid[0][0], grid[1][1], grid[2][2]]
-    d2 = [grid[0][2], grid[1][1], grid[2][0]]
+def isMovesLeft(board):
+    for i in range(3):
+        for j in range(3):
+            if(board[i][j] == '_'):
+                return True
+        return False
 
-    if d1.count(player) == 2 and d1.count('Z') == 1:
-        return ('D', d1.index('Z'), d1.index('Z')) 
-    if d2.count(player) == 2 and d2.count('Z') == 1:
-        return ('D', d2.index('Z'), 2-d1.index('Z')) 
+def evaluate(b) :
+   
+    # Checking for Rows for X or O victory.
+    for row in range(3) :    
+        if (b[row][0] == b[row][1] and b[row][1] == b[row][2]) :       
+            if (b[row][0] == player) :
+                return 10
+            elif (b[row][0] == opponent) :
+                return -10
+ 
+    # Checking for Columns for X or O victory.
+    for col in range(3) :
+      
+        if (b[0][col] == b[1][col] and b[1][col] == b[2][col]) :
+         
+            if (b[0][col] == player) :
+                return 10
+            elif (b[0][col] == opponent) :
+                return -10
+ 
+    # Checking for Diagonals for X or O victory.
+    if (b[0][0] == b[1][1] and b[1][1] == b[2][2]) :
+     
+        if (b[0][0] == player) :
+            return 10
+        elif (b[0][0] == opponent) :
+            return -10
+ 
+    if (b[0][2] == b[1][1] and b[1][1] == b[2][0]) :
+     
+        if (b[0][2] == player) :
+            return 10
+        elif (b[0][2] == opponent) :
+            return -10
+ 
+    # Else if none of them have won then return 0
+    return 0
 
-    return None
-pcn = False
-ptln = False
-def playGame(grid, player, pos, ms, mc):
-    global pcn
-    global ptln
 
-    nz = True
-    for i in grid:
-        if 'Z' in i:
-            nz = False
-            break
-    
-    if nz:
-        return 0, 0
 
-    if grid[pos[0]][pos[1]] != 'Z':
-        return -1, -1
-    
-    if player == ms:
-        grid[pos[0]][pos[1]] = ms
-        return -1, grid
+    def minimax(board, depth, isMax) :
+    score = evaluate(board)
+ 
+    # If Maximizer has won the game return his/her
+    # evaluated score
+    if (score == 10) :
+        return score
+ 
+    # If Minimizer has won the game return his/her
+    # evaluated score
+    if (score == -10) :
+        return score
+ 
+    # If there are no more moves and no winner then
+    # it is a tie
+    if (isMovesLeft(board) == False) :
+        return 0
+ 
+    # If this maximizer's move
+    if (isMax) :    
+        best = -1000
+ 
+        # Traverse all cells
+        for i in range(3) :        
+            for j in range(3) :
+              
+                # Check if cell is empty
+                if (board[i][j]=='_') :
+                 
+                    # Make the move
+                    board[i][j] = player
+ 
+                    # Call minimax recursively and choose
+                    # the maximum value
+                    best = max( best, minimax(board,
+                                              depth + 1,
+                                              not isMax) )
+ 
+                    # Undo the move
+                    board[i][j] = '_'
+        return best
+ 
+    # If this minimizer's move
+    else :
+        best = 1000
+ 
+        # Traverse all cells
+        for i in range(3) :        
+            for j in range(3) :
+              
+                # Check if cell is empty
+                if (board[i][j] == '_') :
+                 
+                    # Make the move
+                    board[i][j] = opponent
+ 
+                    # Call minimax recursively and choose
+                    # the minimum value
+                    best = min(best, minimax(board, depth + 1, not isMax))
+ 
+                    # Undo the move
+                    board[i][j] = '_'
+        return best
 
-    grid[pos[0]][pos[1]] = player
-    res = checkWins(grid, mc)
-    if res is not None:
-        grid[res[1]][res[2]] = mc
-        return (res[1], res[2]), grid
-    res = checkWins(grid, player)
-    if res is not None:
-        grid[res[1]][res[2]] = mc
-        return (res[1], res[2]), grid
+def findBestMove(board) :
+    bestVal = -1000
+    bestMove = (-1, -1)
+ 
+    # Traverse all cells, evaluate minimax function for
+    # all empty cells. And return the cell with optimal
+    # value.
+    for i in range(3) :    
+        for j in range(3) :
+         
+            # Check if cell is empty
+            if (board[i][j] == '_') :
+             
+                # Make the move
+                board[i][j] = player
+ 
+                # compute evaluation function for this
+                # move.
+                moveVal = minimax(board, 0, False)
+ 
+                # Undo the move
+                board[i][j] = '_'
+ 
+                # If the value of the current move is
+                # more than the best value, then update
+                # best/
+                if (moveVal > bestVal) :               
+                    bestMove = (i, j)
+                    bestVal = moveVal
+ 
+    print("The value of the best Move is :", bestVal)
+    print()
+    return bestMove
 
-    print(player, ms)
-    if mc == 0:
-        
-
-        if pos in [(0,0), (0,2), (2,0), (2,2)]:
-            grid[1][1] = mc
-            return (1,1), grid
-
-        if pos == (1,1):
-            grid[2][0] = mc
-            return (2, 0), grid
-        
-        else:
-            grid[1][1] = mc
-            return (1,1), grid
-    elif mc == 1:
-        if pos in [(1,0), (0, 1)]:
-            grid[2][2] = mc
-            pcn = True
-            return (2,2), grid
-        if pos in [(2, 1), (1,2)]:
-            grid[0][0] = mc
-            pcn = True
-            return (0,0), grid
-        if pos in [(0, 0), (2,2)]:
-            grid[0][2] = mc
-            return (0, 2), grid
-        if pos == (0,2):
-            grid[2][2] = mc
-            ptln = True
-            return (2,2), grid
-        if pos == (1,1):
-            grid[0][2] = mc
-            return (0,2), grid
-    elif mc == 3:
-        if pcn:
-            grid[1][1] = mc
-            return (1,1), grid
-        if ptln:
-            grid[0][0] = mc
-            return (0, 0), grid
-    for ri, i in enumerate(grid):
-        if 'Z' in i:
-            u = i.index('Z')
-            grid[ri][u]=mc
-            return (ri, u), grid
 
 def start(battleId):
     s = requests.Session()
-    ms = None
-
-    grid = [list('ZZZ'), list('ZZZ'), list('ZZZ')]
-
-    mc = 0
 
     with s.get(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/{battleId}', headers = None, stream = True) as r:
         for line in r.iter_lines():
@@ -141,26 +199,33 @@ def start(battleId):
                 ms = data["youAre"]
                 
                 if ms == "O":
-                    _, grid = playGame(grid, ms, posMap["SE"], ms, mc)
-                    mc += 1
-                    requests.post(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/play/{battleId}', json={"action": "putSymbol", "position": "SE"})
+                    player,opponent = ['O','X']
+                    bestMove = findBestMove(board)
+                    i,j = bestMove
+                    board[i][j] =  player
+                    pos = posMap[bestMove]
+                    requests.post(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/play/{battleId}', json={"action": "putSymbol", "position": pos})
+                else:
+                    player,opponent = ['X','O']
                 continue
+
             if "player" in data:
                 if data['action'] != 'putSymbol':
                     return
-                if data['player'] == ms:
+                if data['player'] == player:
                     continue
-                player = data['player']
-                pos = posMap[data['position']]
+                opp_pos = data['position']
+                i,j = checkmap[opp_pos]
+                board[i][j] = opponent
 
-                move, grid = playGame(grid, player, pos, ms, mc)
-                if move is None or move == 0:
-                    continue
-                if move == -1:
+                bestMove = findBestMove(board)
+                if bestMove == (-1,-1):
                     requests.post(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/play/{battleId}', json={"action": "(╯°□°)╯︵ ┻━┻"})
                 else:
-                    requests.post(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/play/{battleId}', json={"action": "putSymbol", "position": rMap[move]})
-                mc += 2
+                    i,j = bestMove
+                    board[i][j] =  player
+                    pos = posMap[bestMove]
+                    requests.post(f'https://cis2021-arena.herokuapp.com/tic-tac-toe/play/{battleId}', json={"action": "putSymbol", "position": pos})
             else:
                 return
 
